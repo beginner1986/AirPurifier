379e22f4b48ed6b15ba863c9706b122e35663625
Adam Emieljaniuk <beginner1986@gmail.com>
2020-04-11 10:02:48 +0200

    Add project files.

---
+++ AirPurifier/AirPurifier.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		AirPurifier.ino
+ Created:	4/11/2020 10:02:18 AM
+ Author:	begin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
