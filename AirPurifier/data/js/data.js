b1bd5b00c9e75e25331c616a910d71fd75f84668
Adam Emieljaniuk <beginner1986@gmail.com>
2020-04-23 06:49:41 +0200

    Sensor data printed with color depending of their value.

--- AirPurifier/data/js/data.js
+++ AirPurifier/data/js/data.js
@@ -21,12 +21,62 @@ function process() {
 function handleServerResponse() {
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
         xmlResponse = xmlHttp.responseXML;
+
         xmldoc = xmlResponse.getElementsByTagName('response');
-        document.getElementById('pm1').innerHTML = xmldoc[0].getElementsByTagName('pm1')[0].childNodes[0].nodeValue + " &microg/m<sup>3</sup>";
-        document.getElementById('pm2_5').innerHTML = xmldoc[0].getElementsByTagName('pm2_5')[0].childNodes[0].nodeValue + " &microg/m<sup>3</sup>";
-        document.getElementById('pm10').innerHTML = xmldoc[0].getElementsByTagName('pm10')[0].childNodes[0].nodeValue + " &microg/m<sup>3</sup>";
-        document.getElementById('temperature').innerHTML = xmldoc[0].getElementsByTagName('temperature')[0].childNodes[0].nodeValue + " &deg;C";
-        document.getElementById('pressure').innerHTML = xmldoc[0].getElementsByTagName('pressure')[0].childNodes[0].nodeValue + " hPa";
-        document.getElementById('humidity').innerHTML = xmldoc[0].getElementsByTagName('humidity')[0].childNodes[0].nodeValue + " %";
+        var pm1 = xmldoc[0].getElementsByTagName('pm1')[0].childNodes[0].nodeValue;
+        var pm2_5 = xmldoc[0].getElementsByTagName('pm2_5')[0].childNodes[0].nodeValue;
+        var pm10 = xmldoc[0].getElementsByTagName('pm10')[0].childNodes[0].nodeValue;
+        var temperature = xmldoc[0].getElementsByTagName('temperature')[0].childNodes[0].nodeValue;
+        var pressure = xmldoc[0].getElementsByTagName('pressure')[0].childNodes[0].nodeValue;
+        var humidity = xmldoc[0].getElementsByTagName('humidity')[0].childNodes[0].nodeValue;
+
+        document.getElementById('pm1').innerHTML = pm1 + " &microg/m<sup>3</sup>";
+        document.getElementById('pm2_5').innerHTML = pm2_5 + " &microg/m<sup>3</sup>";
+        document.getElementById('pm10').innerHTML = pm10 + " &microg/m<sup>3</sup>";
+        document.getElementById('temperature').innerHTML = temperature + " &deg;C";
+        document.getElementById('pressure').innerHTML = pressure + " hPa";
+        document.getElementById('humidity').innerHTML = humidity + " %";
+
+        if (pm1 > 50) {
+            document.getElementById('pm1').style.color = 'red';
+        } else if (pm1 > 20) {
+            document.getElementById('pm1').style.color = '#ff6a00';
+        } else {
+            document.getElementById('pm1').style.color = 'black';
+        }
+
+        if (pm2_5 > 50) {
+            document.getElementById('pm2_5').style.color = 'red';
+        } else if (pm2_5 > 20) {
+            document.getElementById('pm2_5').style.color = '#ff6a00';
+        } else {
+            document.getElementById('pm2_5').style.color = 'black';
+        }
+
+        if (pm10 > 50) {
+            document.getElementById('pm10').style.color = 'red';
+        } else if (pm10 > 205) {
+            document.getElementById('pm10').style.color = '#ff6a00';
+        } else {
+            document.getElementById('pm10').style.color = 'black';
+        }
+
+        if (humidity >= 70) {
+            document.getElementById('humidity').style.color = 'red';
+        } else {
+            document.getElementById('humidity').style.color = 'black';
+        }
+
+        if (temperature <= -10 || temperature >= 50) {
+            document.getElementById('temperature').style.color = 'red';
+        } else {
+            document.getElementById('temperature').style.color = 'black';
+        }
+
+        if (pressure <= 860 || pressure >= 1100) {
+            document.getElementById('temperature').style.color = 'red';
+        } else {
+            document.getElementById('temperature').style.color = 'black';
+        }
     }
 }
